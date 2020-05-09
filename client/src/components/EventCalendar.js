56e926fb65651a33ce56bb4c43d4541c58a81a31
Meredith Barnett <meredithbarnettcontact@gmail.com>
2020-05-09 11:16:48 -0600

    added to postbuild instructions

--- client/src/components/EventCalendar.js
+++ client/src/components/EventCalendar.js
@@ -3,7 +3,7 @@ import '../App.css';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 
-import API from '../Utils/api';
+import API from '../utils/api';
 
 
 import {Modal, ModalHeader, ModalBody} from "reactstrap";
