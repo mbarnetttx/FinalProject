03e83cde91590934fb645b078a36039264641321
Meredith Barnett <meredithbarnettcontact@gmail.com>
2020-05-12 17:43:00 -0600

    try fix routing issue

--- client/src/components/EventCalendar.js
+++ client/src/components/EventCalendar.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import '../App.css';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import API from '../Utils/api';
+import API from '../utils/api';
 
 
 import {Modal, ModalHeader, ModalBody} from "reactstrap";
