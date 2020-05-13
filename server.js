c964927585da4962092307a584a21b4e64b4cc9b
estivensal7 <estivensal7@gmail.com>
2020-05-12 23:22:46 -0400

    updating server.js file to check if in production

--- server.js
+++ server.js
@@ -1,44 +1,47 @@
-if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
 }
 
-const express    = require('express');
-const app        = express();
-const bodyParser = require('body-parser') // middleware that parses data
-const cors       = require('cors') // middleware to access the routes
-const bcrypt = require('bcrypt')
-const mongoose   = require("mongoose");
-const routes     = require("./routes");
-const PORT       = process.env.PORT || 3001;
-const Users      = require('./routes/Users')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser"); // middleware that parses data
+const cors = require("cors"); // middleware to access the routes
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const PORT = process.env.PORT || 3001;
+const Users = require("./routes/Users");
 
 // middleware
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.use(bodyParser.json())
-app.use(cors())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
 app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-)
+	bodyParser.urlencoded({
+		extended: false,
+	})
+);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+}
 
 // Add routes, both API and view
 // const donationsRouter = ("./controllers/donationsController")
 // app.use(donationsRouter);
 
-app.use('/users', Users)
+app.use("/users", Users);
 app.use(routes);
 
-
 // Connect to the Mongo DB
-mongoose.connect(
-	process.env.MONGODB_URI || "mongodb://localhost:27017/donations",
-	{ useUnifiedTopology: true, useNewUrlParser: true }
-)
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err))
-
+mongoose
+	.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/donations", {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+	})
+	.then(() => console.log("MongoDB Connected"))
+	.catch((err) => console.log(err));
 
 // Start the API server
 app.listen(PORT, function () {
