e920c5d424e405e0e27ab5ac9f9f4834af7205e3
Meredith Barnett <meredithbarnettcontact@gmail.com>
2020-05-12 18:37:05 -0600

    fixing routes

---
+++ client/build/precache-manifest.9907998c1f5abae3350631d2316e7e68.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25cf079e35d3418215ac4981ba0b2d74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ccd0d533f0c680bb6d5",
+    "url": "/static/css/2.8b176400.chunk.css"
+  },
+  {
+    "revision": "0fb83bc461575a10cab5",
+    "url": "/static/css/main.a90aa7d6.chunk.css"
+  },
+  {
+    "revision": "8ccd0d533f0c680bb6d5",
+    "url": "/static/js/2.4badf8ea.chunk.js"
+  },
+  {
+    "revision": "7caa50ecbd1c182243dc77f12757b8a9",
+    "url": "/static/js/2.4badf8ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb83bc461575a10cab5",
+    "url": "/static/js/main.889402e6.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "00b770b9815cb4421c34c9a7b1b40cdb",
+    "url": "/static/media/meredith.00b770b9.png"
+  },
+  {
+    "revision": "06b70609af70ba1547581c5bee47f1f7",
+    "url": "/static/media/neighborlogo.06b70609.png"
+  },
+  {
+    "revision": "0c8e0291ebdb0fc2e6ac430bb27b96e3",
+    "url": "/static/media/neighborlogosmall.0c8e0291.png"
+  }
+]);
