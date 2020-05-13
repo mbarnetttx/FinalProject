adb8d3f00ed704db1d8744af7fc702a152fd556f
estivensal7 <estivensal7@gmail.com>
2020-05-13 00:29:42 -0400

    adding proxy again

--- server.js
+++ server.js
@@ -21,11 +21,10 @@ app.use(
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
+	app.get("*", (request, response) => {
+		response.sendFile(path.join(__dirname, "client/build", "index.html"));
+	});
 }
-
-app.get("*", (request, response) => {
-	response.sendFile(path.join(__dirname, "client/build", "index.html"));
-});
 
 // Add routes, both API and view
 // const donationsRouter = ("./controllers/donationsController")
